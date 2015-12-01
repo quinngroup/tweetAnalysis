b609230b9e18e0fe6b3a4ed2bf3a6df54bd53b9b
ranjanmanish <cse1.manish@gmail.com>
2015-12-01 18:03:00 -0500

    changed java file to add friends to set to find unique ones

--- FriendAnalysis/GetUserSet.java
+++ FriendAnalysis/GetUserSet.java
@@ -1,36 +1,54 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Set;
+
 
 
 public class GetUserSet {
-public static void main(String[] args) {
-	String fileName = "/home/manish/ResearchWork/RA/Tweet_Friends/Friends/resultOutputImportedFromAws/experiment.txt";
-	try{
-		
-		//Create object of FileReader
-		FileReader inputFile = new FileReader(fileName);
+	private static final String DELIMETER = ",";
 
-		//Instantiate the BufferedReader Class
-		BufferedReader bufferReader = new BufferedReader(inputFile);
+	public static void main(String[] args) {
+		Set<String> friendSet = new HashSet<String> ();
+		String fileName = "/home/manish/ResearchWork/RA/Tweet_Friends/Friends/resultOutputImportedFromAws/experiment.txt";
+		try{
 
-		//Variable to hold the one line data
-		String line;
-		
-		// Read file line by line and print on the console
-		while ((line = bufferReader.readLine()) != null)   {
-			//System.out.println(line);
-			// Data format 1100          SimonGire
-                        if(!line.startsWith("$#$"))
-			    System.out.println(line.trim());
-                        else{
-                            line = line.substring(3, line.length());
-			    System.out.println(line.trim());
-                        }
+			//Create object of FileReader
+			FileReader inputFile = new FileReader(fileName);
+
+			//Instantiate the BufferedReader Class
+			BufferedReader bufferReader = new BufferedReader(inputFile);
+
+			//Variable to hold the one line data
+			String line;
+			String[] friendArray;
+			int counter =0;
+			
+			// Read file line by line and print on the console
+			while ((line = bufferReader.readLine()) != null)   {
+				//System.out.println(line);
+				// Data format 1100          SimonGire
+				if(!line.startsWith("$#$"))
+					System.out.println(line.trim());
+				else{
+					line = line.substring(3, line.length());
+					friendArray = line.trim().split(DELIMETER);
+					for(String str: friendArray){
+						counter++;
+						friendSet.add(str);
+					}
+				}
+			}
+			for(String temp:friendSet){
+				System.out.println(temp);
+			}
+			System.out.println("Actual"+counter);
+			System.out.println("After Reduction"+friendSet.size());
+			
+			bufferReader.close();
 		}
-		bufferReader.close();
+		catch(Exception e){
+			System.out.println("Error while reading file line by line:" + e.getMessage());
+		}
 	}
-	catch(Exception e){
-		System.out.println("Error while reading file line by line:" + e.getMessage());
-	}
-}
 }
