3497b770f80ee4699541069c3390501ac9706920
Manish <cse1.manish@gmail.com>
2016-02-02 19:41:43 -0500

    auth files info delted

--- FollowerAnalysis/authinfo1.py
+++ FollowerAnalysis/authinfo1.py
@@ -3,5 +3,5 @@ consumer_secret="YOU NEDD TO FIND YOUR OWN ACCESS KEY FOR THIS"
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_key="96058036-nvf5aKMAKk8J2ITWsExEqmNgxYmp9QAQlMQUER5mj"
-access_secret="96BvNtHt6kLaM6GQqJa29WwKWkBkJ09QvF2ILGlXLTs09"
+access_key="96058036-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxx"
+access_secret="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx"
