42aaeaed5159015d9da612947cb3bb2ad718082a
Manish Ranjan <cse1.manish@gmail.com>
2016-02-15 17:04:51 -0500

    This change in script analyses the network intercation while replying for Non Filtered users

---
+++ RetweetReplyAnalysis/createReplyGraph.py
@@ -0,0 +1,52 @@
+#@author ranjanmanish
+#from __future__ import print_function
+import re
+import sys
+import os
+
+fileOfUser = open("userNF.txt")
+usersNF = []
+for user in fileOfUser:
+	# strip() is required to get rid of new line at the end
+	user = user.strip()
+	usersNF.append(user)
+
+def getRetweetsGraph(filename, fileAdd):
+	user = fileName.split(".")[0]
+	retweetList = ""
+	fileContent = open(fileAdd)
+	try:
+		for line in fileContent:
+			try:
+				tweetText = line.split(",")[2]
+				tweetText = tweetText.strip()
+                                #split based on whitespace
+				words = tweetText.split()
+                                # get rid of whitespaces
+				word1 = words[0].strip()
+                                # get rid of extra " at the begining
+                                word1 = word1.replace("\"", "")
+                                #get the first char based on decision if this is a reply can be taken
+                                firstChar = word1[0]
+				# the the exact userName i.e from @manishranjan it will give manishranjan
+				word1 = word1[1:]
+                                if firstChar == '@':
+					# if the userName is in the list of users => to find how the user interacts within his/her network
+					if word1 in usersNF:
+						retweetList = retweetList + ";" + word1
+			except :
+				pass
+	except:
+		pass
+	print user + retweetList
+
+#main definition here
+if __name__  == '__main__':
+    path = os.getcwd()
+    #path = path +"/TweetOfFiltered"
+    #path = path +"/CSVs_NF"
+    path = path +"/CSVs_NF"
+    lst=os.listdir(path)
+    for fileName in lst:
+	    fileAdd = path + "/" +fileName
+	    getRetweetsGraph(fileName,fileAdd)
