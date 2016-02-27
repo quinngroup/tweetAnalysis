62f0ffbee8a0c1fc85e3708fc37c520865c10a0b
ranjanmanish <cse1.manish@gmail.com>
2016-02-27 17:33:45 -0500

    Script to compute TFIDF of all the tweets

---
+++ TFIDF/tfidf_F.py
@@ -0,0 +1,55 @@
+import math
+import os
+from textblob import TextBlob as tb
+
+def tf(word, blob):
+	return (float)(blob.words.count(word)) / (float)(len(blob.words))
+
+def n_containing(word, bloblist):
+    return sum(1 for blob in bloblist if word in blob)
+
+def idf(word, bloblist):
+    return math.log(len(bloblist) / (float)(1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+    return tf(word, blob) * idf(word, bloblist)
+
+'''document3 = tb("""The Colt Python is a .357 Magnum caliber revolver formerly
+manufactured by Colt's Manufacturing Company of Hartford, Connecticut.
+It is sometimes referred to as a "Combat Magnum".[1] It was first introduced
+in 1955, the same year as Smith & Wesson's M29 .44 Magnum. The now discontinued
+Colt Python targeted the premium revolver market segment. Some firearm
+collectors and writers such as Jeff Cooper, Ian V. Hogg, Chuck Hawks, Leroy
+Thompson, Renee Smeets and Martin Dougherty have described the Python as the
+finest production revolver ever made.""")'''
+
+
+def getTFIDF(bloblist):
+    for i, blob in enumerate(bloblist):
+        print("Top words in document {}".format(i + 1))
+        scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        for word, score in sorted_words[:50]:
+            print("Word: {}, TF-IDF: {}".format(word, round(score, 5)))
+
+
+# main here
+if __name__ == '__main__':
+    documentList = []
+    path = os.getcwd()
+    #path = path + "/OUT_F/"
+    path = path + "/TEMP/"
+    lst = os.listdir(path)
+    for fileName in lst:
+        temp = ""
+        fileAdd = path + fileName
+        fileContent = open(fileAdd)
+        for line in fileContent:
+            try:
+                temp = temp + " " + line
+            except:
+                pass
+
+        document1 = tb(temp)
+        documentList.append(document1)
+    getTFIDF(documentList)
