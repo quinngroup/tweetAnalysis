5a666c7389c93edc68f5f3dbc69e95ea01bea6b2
ranjanmanish <cse1.manish@gmail.com>
2016-03-05 17:55:33 -0500

    Changes in the dictionary created with extra filters on top of what comes with nltk

--- LDA/getLDAForTweets.py
+++ LDA/getLDAForTweets.py
@@ -1,15 +1,22 @@
 from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 import gensim
 import os
+import re
 #author: https://rstudio-pubs-static.s3.amazonaws.com/79360_850b2a69980c4488b1db95987a24867a.html
 #modified: ranjanmanish
 tokenizer = RegexpTokenizer(r'\w+')
 
 # create English stop words list
 en_stop = get_stop_words('en')
+
+cachedStopWords = stopwords.words("english")
+
+#increasing the stop word list as the twitter data set is noisy
+en_stop.extend(["demibestfans2016", "u", "rt", "t", "s", "updat", "channel", "de", "que", "la", "en", "eurekamag", "na", "sa", "ang", "keo", "ka", "lang", "le", "je", "est", "c", "pa", "j", "ik", "un", "et", "il", "wt", "fpjb", "fnfjb", "rbjb", "amp", "ini", "ada", "amant", "pushawardskathniel", "kathniel", "00", "05", "04", "15", "16", "14", "18", "aku", "niond", "da", "ich", "ero", "rtandfollow", "da", "ich", "und", "ist", "ero", "m", "da", "com", "em", "um", "meu", "na", "pra", "weather", "properti", "googl", "0mm", "co", "thttps", "https", "http", "n", "t", "u", "for", "us", "is:", "it.", "on", "i'll", "also", "of", "via", "follow", "mali", "rt", "got", "nowplay", "periscop", "stat", "replay", "katch", "biztip", "via", "radio", "commerci", "na", "sa", "ang", "ko", "ng", "mo", "aka", "ka", "ve", "ke", "kixmi", "capricorn", "tarnat", "today", "sagittariu", "tauru", "votingdevonne23", "ff", "0", "new", "go", "mm", "aku", "yang", "yg", "ni", "tak", "ada", "nak", "ya", "dutchschultz", "strictlybid", "lovat", "iheartaward", "bestfanarmi", "que", "la", "el", "en", "y", "lo", "es", "ain", "wit", "votingdevonne23", "giveyourheartdd", "amant", "ootd", "bandana", "updat", "get", "channel", "pushawardskathniel", "kathniel", "cbb", "uri", "santiago", "ain'", 'ain'])
 
 # Create p_stemmer of class PorterStemmer
 p_stemmer = PorterStemmer()
@@ -20,7 +27,8 @@ doc_set=[]
 # get the current path
 path = os.getcwd()
 # bring the path to speed
-path = path + "/TEMP"
+path = path + "/ALLIN"
+#path = path + "/TEMP"
 
 #get the list of files
 lst = os.listdir(path)
@@ -33,41 +41,41 @@ for fileName in lst:
     for line in fileContent:
         try:
             line = line.split(",")[2]
-            print line
-            line = re.sub(r'#([^\s]+)', r'\1', line)
             line = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',line)
+            line = re.sub(r'#([^\s]+)', r'\1', line)
+            line = re.sub('@[^\s]+','', line)
             line = line.strip('\'"')
-            #print line
+            # and this was throwing exception because of unicode error
+            line = unicode(line, "utf8")
             temp = temp + str(line)
             #print temp
         except:
             pass
     doc_set.append(temp)
 
-print len(doc_set)
-
-# compile sample documents into a list
-#doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
-print type(doc_set)
-
 # list for tokenized documents in loop
 texts = []
 
 # loop through document list
 for i in doc_set:
-    
+    filtered_tokens = []
     # clean and tokenize document string
     raw = i.lower()
     tokens = tokenizer.tokenize(raw)
-
     # remove stop words from tokens
     stopped_tokens = [i for i in tokens if not i in en_stop]
     
+    #stopped_tokens = [i for i in tokens if not i in cachedStopWords]
+    
     # stem tokens
     stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
+    for element in stemmed_tokens:
+        if len(element) > 3:
+            filtered_tokens.append(element)
     
     # add tokens to list
-    texts.append(stemmed_tokens)
+    texts.append(filtered_tokens)
 
 # turn our tokenized documents into a id <-> term dictionary
 dictionary = corpora.Dictionary(texts)
@@ -76,6 +84,6 @@ dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 # generate LDA model
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word = dictionary, passes=30)
 
-print(ldamodel.print_topics(num_topics=2))
+print(ldamodel.print_topics(num_topics=10))
