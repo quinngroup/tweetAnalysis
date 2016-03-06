32959bac15d8fa6e6d079aec3b369d63750e83e0
ranjanmanish <cse1.manish@gmail.com>
2016-03-06 13:49:27 -0500

    Proof of concept for finding document frequency from the trauined lda model

--- LDA/getLDASample.py
+++ LDA/getLDASample.py
@@ -1,7 +1,10 @@
 from nltk.tokenize import RegexpTokenizer
 from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
+from collections import defaultdict
 from gensim import corpora, models
+from pprint import pprint
+
 import gensim
 
 tokenizer = RegexpTokenizer(r'\w+')
@@ -17,37 +20,94 @@ doc_a = "Brocolli is good to eat. My brother likes to eat good brocolli, but not
 doc_b = "My mother spends a lot of time driving my brother around to baseball practice."
 doc_c = "Some health experts suggest that driving may cause increased tension and blood pressure."
 doc_d = "I often feel pressure to perform well at school, but my mother never seems to drive my brother to do better."
-doc_e = "Health professionals say that brocolli is good for your health." 
+doc_e = "Health professionals say that brocolli is good for your health."
 
+doc_f = "I was playing sport and it was sexy sports"
+
+doc_g = "health professionals always suggest my mother  to play sportss and eat brocoli for his increased tension as well as sporty"
+
+doc_h = "health sports mother brocolli "
 # compile sample documents into a list
 doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
+
+doc_test = [doc_f, doc_g, doc_h]
 
 # list for tokenized documents in loop
 texts = []
 
 # loop through document list
 for i in doc_set:
+    # initialize fre
+    #frequency = defaultdict(int)
     
     # clean and tokenize document string
     raw = i.lower()
     tokens = tokenizer.tokenize(raw)
-
     # remove stop words from tokens
     stopped_tokens = [i for i in tokens if not i in en_stop]
     
     # stem tokens
     stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
     
     # add tokens to list
     texts.append(stemmed_tokens)
 
+'''for text in texts:
+    for token in text:
+        frequency[token] += 1
+
+texts = [[token for token in text if frequency[token] > 1] for text in texts]'''
+
+
+pprint(texts)
+
 # turn our tokenized documents into a id <-> term dictionary
 dictionary = corpora.Dictionary(texts)
-    
+
+print dictionary
+
 # convert tokenized documents into a document-term matrix
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 # generate LDA model
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word = dictionary, passes=20)
 
 print(ldamodel.print_topics(num_topics=2, num_words=10))
+
+texts = []
+
+stopeed_tokens = []
+stemmed_tokens = []
+
+for i in doc_test:
+
+    raw = i.lower()
+    tokens = tokenizer.tokenize(raw)
+
+    # remove stop words from tokens
+    stopped_tokens = [i for i in tokens if not i in en_stop]
+
+    # stem tokens
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
+    texts.append(stemmed_tokens)
+
+
+'''for text in texts:
+    for token in text:
+        frequency[token] += 1
+
+texts = [[token for token in text if frequency[token] > 1] for text in texts]'''
+
+pprint (texts)
+
+dictionary = corpora.Dictionary(texts)
+
+#doc_bow = [(0, 1), (1, 1)]
+
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+for item in corpus:
+    print ldamodel[item]
+
