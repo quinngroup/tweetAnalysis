7bd75894a7084bdf1d13015f881b6277ff9e6261
ranjanmanish <cse1.manish@gmail.com>
2016-03-11 16:37:58 -0500

    Adding temp file tow ork from another system

--- LDA/Results/convert2Feature.py
+++ LDA/Results/convert2Feature.py
@@ -2,15 +2,34 @@
 
 import os
 import sys
+import json
+
 def converDFileToFeature(path):
     """
     the job of this function is to, for a given input file convert them in to feature set 
     which could be consumed by any ML clustering algorithm
     """
+    if os.path.exists(path):
+        fileContent = open(path)
+        
+        # skip  first line as that has print of all the models
+        next(fileContent)
+
+        # now traversing file content one by one
+        for line in fileContent:
+            user, feature = line.split(" ", 1)
+            feature = eval(feature)
+            tempFeature = []
+            for var in feature:
+
+                
+    else:
+        print "The file was not found , Fatal Error, Exiting!!!"
+
 
 # main feature
 if __name__ == '__main__':
-    fileName = sys.argsv[1]
+    fileName = sys.argv[1]
     path = os.getcwd()
     path = path + "/" + fileName 
     converDFileToFeature(path)
