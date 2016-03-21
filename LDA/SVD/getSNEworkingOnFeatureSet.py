705e4b2df9d2948a2aef4da69ee1656b85586c3c
ranjanmanish <cse1.manish@gmail.com>
2016-03-20 22:25:28 -0400

    tSNE for all the feature vectors

--- LDA/SVD/getSNEworkingOnFeatureSet.py
+++ LDA/SVD/getSNEworkingOnFeatureSet.py
@@ -13,13 +13,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 
 from sklearn.preprocessing import StandardScaler
 from scipy.linalg import svd
 from sklearn.decomposition import TruncatedSVD
 
 from sklearn.manifold import TSNE
+from IPython.html.widgets import interactive, fixed
 
 
 #take fileName as input
@@ -46,10 +47,10 @@ def dataPropPrint(fileName):
     plt.show()
 
 def tSNE(fileName):
-    cnx = sqlite3.connect('data/50Feature.db')
+    cnx = sqlite3.connect('data/40Feature.db')
     dforigtrain = pandas.read_csv(fileName)
-    #print (mydata.shape)
-    #print mydata.head()
+    print (dforigtrain.shape)
+    print dforigtrain.head()
     dforigtrain.rename(columns=lambda x: '_'.join([x.strip() for x in x.lower().split()]), inplace=True)
     df = dforigtrain[[c for c in dforigtrain.columns.values.tolist() if c != 'orig_set']]
     print(df.shape)
@@ -59,6 +60,7 @@ def tSNE(fileName):
 
     df = pd.read_sql('select * from df_clean', cnx)
 
+    print (df.shape)
     scaler = StandardScaler().fit(df.iloc[:,2:])
 
     dfs = pd.DataFrame(scaler.transform(df.iloc[:,2:]), index=df.index, columns=df.columns[2:])
@@ -72,12 +74,12 @@ def tSNE(fileName):
     print('{} SVs less than 1e-12'.format(len(s[s < 1e-12])))
     
     plt.show()
-
+    '''
     # from here the Truncated SVD , this is mostly helpful in image data set where reducing dimensions is mostl;y possibel . In our case, every feature was contributing . Hence no Truncation is possible
    
-    ncomps = 48
+    ncomps = 39
 
-    svd = TruncatedSVD(algorithm='randomized', n_components = 48)
+    svd = TruncatedSVD(algorithm='randomized', n_components = ncomps)
     
     svd_fit = svd.fit(dfs)
     
@@ -91,24 +93,72 @@ def tSNE(fileName):
     #plt.show()
 
     dfsvd = pd.DataFrame(Y, columns=['c{}'.format(c) for c in range(ncomps)], index=df.index)
-    print(dfsvd.shape)
-    #dfsvd.head()
     
     dfsvd.to_sql('df_svd',cnx,if_exists='replace', index=None)
 
     dfsvd = pd.read_sql('select * from df_svd', cnx)
 
+    
+    print(dfsvd.shape)
+    
     svdcols = [c for c in dfsvd.columns if c[0] == 'c']
 
     df = pd.read_sql('select * from df_clean', cnx)
+    
+    print(dfsvd.shape)
+#
+    print (dfsvd.head())
+    
+    #rowsubset = [10,20,40,80,160,320,640, 1280, 1900]
+    tsne = TSNE(n_components=2, random_state=0)
+    '''runs = np.empty((len(rowsubset),1))
 
-    plotdims = 5
-    ploteorows = 1
-    dfsvdplot = dfsvd[svdcols].iloc[:,:plotdims]
-    dfsvdplot['class'] = df['class']
-    ax = sns.pairplot(dfsvdplot.iloc[::ploteorows,:], hue='class', size=1.8)
+    for i, rows in enumerate(rowsubset):
+        t0 = time()
+        Z = tsne.fit_transform(dfsvd.iloc[:rows,:][svdcols])
+        runs[i] = time() - t0
 
+    ax = pd.DataFrame(runs, index=rowsubset).plot(kind='bar', logy=False, figsize=(10,4))
     plt.show()'''
+    
+    
+    Z = tsne.fit_transform(dfsvd[svdcols])
+    dftsne = pd.DataFrame(Z, columns=['x','y'], index=dfsvd.index)
+    ax = sns.lmplot('x', 'y', dftsne, fit_reg=False, size=8
+                            ,scatter_kws={'alpha':0.7,'s':60})
+
+    
+    #plt.show()
+
+    
+    dftsne['class'] = df['class']
+    g = sns.lmplot('x', 'y', dftsne, hue='class', fit_reg=False, size=8
+                            ,scatter_kws={'alpha':0.7,'s':60})
+    g.axes.flat[0].set_title('Scatterplot of a 50D dataset reduced to 2D using t-SNE')
+
+    plt.show()
+
+def plot_3d_scatter(A, elevation=30, azimuth=120):
+    """ Create 3D scatterplot """
+    
+    maxpts=1000
+    fig = plt.figure(1, figsize=(9, 9))
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=elevation, azim=azimuth)
+    ax.set_xlabel('component 0')
+    ax.set_ylabel('component 1')
+    ax.set_zlabel('component 2')
+
+    # plot subset of points
+    rndpts = np.sort(np.random.choice(A.shape[0], min(maxpts,A.shape[0]), replace=False))
+    coloridx = np.unique(A.iloc[rndpts]['class'], return_inverse=True)
+    colors = coloridx[1] / len(coloridx[0])   
+    
+    sp = ax.scatter(A.iloc[rndpts,0], A.iloc[rndpts,1], A.iloc[rndpts,2]
+               ,c=colors, cmap="jet", marker='o', alpha=0.6
+               ,s=50, linewidths=0.8, edgecolor='#BBBBBB')
+
+    plt.show()
+
 
 if __name__ == '__main__':
     fileName = sys.argv[1]
