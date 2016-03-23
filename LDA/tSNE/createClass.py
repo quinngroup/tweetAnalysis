1b3f1295c747eb58967a8fa1845a6275617d43d9
ranjanmanish <cse1.manish@gmail.com>
2016-03-23 16:33:03 -0400

    adding the actual create class code

--- LDA/tSNE/createClass.py
+++ LDA/tSNE/createClass.py
@@ -1,17 +1,30 @@
+# author ranjanmanish
 import sys
 
 fileName = sys.argv[1]
 fp = open(fileName)
-median = 0.34
-
+next (fp)
+std_10 = 0.34
+std_20 = 0.32
+std_30 = 0.22
+std_40 = 0.20
+std_50 = 0.21
+counter = 1
 for line in fp:
     line = line.strip()
-    userName = line.split(",")[0]
-    val = line.split(",")[8]
+    lineList = line.split(",")[1:]
+    #userName = line.split(",")[0]
+    #val = lineList[7]
+    #val = lineList[0]
+    #val = lineList[19]
+    val = lineList[3]
     #print userName, val
     val = float(val)
-    if  val >= median:
-        print line+",N"
+    #print val, type(val)
+    myList = ','.join(map(str, lineList))
+    if  val >= std_50:
+        print "N,"+ str(counter) +"," +myList
+        counter = counter + 1
     else:
-        print line+",NN"
-
+        print "NN,"+  str(counter) + "," +myList
+        counter = counter + 1
